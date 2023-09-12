9d6268df72ed8ef9b8d3c4dd3278675604bf3a19
okok111 <1for.rails@gmail.com>
2023-09-12 15:34:53 +0900

    経路表示参考コード

---
+++ app/controllers/directions_controller.rb
@@ -0,0 +1,17 @@
+class DirectionsController < ApplicationController
+  def new
+  end
+  def create
+    # waypointは最大４つなので固定にする
+    # TODO：waypointの数を可変にして、createメソッドを削除する
+    waypoints = [params[:waypoint1], params[:waypoint2], params[:waypoint3], params[:waypoint4]]
+
+    redirect_to result_path(start_location: params[:start_location], end_location: params[:end_location], waypoint: waypoints)
+  end
+
+  def result
+    @start_location = params[:start_location]
+    @end_location = params[:end_location]
+    @waypoint = params[:waypoint].split('/')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
+  get '/', to: 'directions#new'
+  post '/directions', to: 'directions#create', as: 'create_direction'
+  get '/result/:start_location/:end_location/:waypoint', to: 'directions#result', as: 'result'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts do
-    resources :likes, only: [:create, :destroy]
-    resources :comments, only: [:create]
-  end                                           
-  root 'posts#index'
+  # resources :posts do
+  #   resources :likes, only: [:create, :destroy]
+  #   resources :comments, only: [:create]
+  # end
+  # root 'posts#index'
 end
